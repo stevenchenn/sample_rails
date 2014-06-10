be768124ded806482d8d60434f08449f2785f58f
Schen <schen@SChen.local>
2014-06-10 16:11:52 -0700

    Add error page

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -29,4 +29,14 @@ module SessionsHelper
   def current_user?(user)
     user == current_user
   end
+
+  def store_location
+    session[:return_to] = request.url if request.get?
+  end
+
+  def redirect_back_or(default)
+    redirect_to(session[:redirect_to] || default)
+    session.delete(:return_to)
+  end 
+
 end
