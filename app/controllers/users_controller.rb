a4ea236d78ff2e05528b68353ee5c2126c575dba
Schen <schen@SChen.local>
2014-06-10 17:02:57 -0700

    Precompile asset

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,17 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      sign_in @user
+      flash[:success] = "Welcome to the Sample App"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end 
+
   def edit
     @user = User.find(params[:id])
   end
